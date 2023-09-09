ca2b839de7416a30486624aacccec8257b1b4128
legitimuzbr <141097352+legitimuzbr@users.noreply.github.com>
2023-09-09 21:37:23 +0000

    t

--- pages/teste.js
+++ pages/teste.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 
 class CameraApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       stream: null,
+      facingMode: "user", // Inicialmente, usando a câmera frontal
     };
     this.videoRef = React.createRef();
   }
 
   componentDidMount() {
+    this.initCamera();
+  }
+
+  initCamera = () => {
     // Solicitar permissão para a câmera
     navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: "user" } }) // Acesso à câmera frontal
+      .getUserMedia({ video: { facingMode: this.state.facingMode } })
       .then((stream) => {
         this.setState({ stream });
         if (this.videoRef.current) {
@@ -22,7 +29,21 @@ class CameraApp extends Component {
       .catch((error) => {
         console.error("Erro ao acessar a câmera: ", error);
       });
-  }
+  };
+
+  toggleCamera = () => {
+    // Alternar entre a câmera frontal e traseira
+    const newFacingMode =
+      this.state.facingMode === "user" ? "environment" : "user";
+    this.setState({ facingMode: newFacingMode }, () => {
+      if (this.state.stream) {
+        this.state.stream.getTracks().forEach((track) => {
+          track.stop();
+        });
+      }
+      this.initCamera();
+    });
+  };
 
   componentWillUnmount() {
     // Parar a câmera ao desmontar o componente
@@ -37,11 +58,15 @@ class CameraApp extends Component {
     return (
       <div>
         <h1>Aplicativo da Câmera</h1>
+        <button onClick={this.toggleCamera}>
+          <FontAwesomeIcon icon={faSync} />
+          Alternar Câmera
+        </button>
         <video
           ref={this.videoRef}
           autoPlay
           playsInline
-          muted // Evita feedback de áudio
+          muted
           style={{ width: "100%", maxWidth: "400px" }}
         />
       </div>
