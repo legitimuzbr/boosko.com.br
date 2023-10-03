858891f37b01192df0575055ffeaf46374ef6b39
legitimuzbr <141097352+legitimuzbr@users.noreply.github.com>
2023-10-03 20:10:01 +0000

    a

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <>
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <h2>Digite seu cpf abaixo:</h2>
+            <input
+              type="number"
+              maxlength="11"
+              className="form-control mt-3"
+              id="legitimuz-hydrate-cpf"
+              placeholder="Digite seu CPF aqui..."
+            />
+            <button
+              className="btn btn-success btn-lg btn-block mt-3 font-weight-bold"
+              type="button"
+              id="legitimuz-action-verify"
+            >
+              VERIFICAR IDENTIDADE
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="legitimuz-root"></div>
+    </>
+  );
+}
+
+export default App;
